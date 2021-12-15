e638b283123f67ec3f77e5b6188bf5f3d7fcfcbc
tomouellette <t.ouellette@mail.utoronto.ca>
2021-12-14 19:23:50 -0500

    Adding an example

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-# Miscellaneous script for building pybind11 python wrappers
-
-# Haploid Wright Fisher simulations
 g++ -O3 -Wall -shared -std=c++17 -undefined dynamic_lookup -fPIC $(python3 -m pybind11 --includes) sv_evolution.cpp -o sv_evolution$(python3-config --extension-suffix) -I ./pybind11/include
